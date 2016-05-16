65562d56024ecf69b0127730cc4009022ff1a577
paulbrunache <paulbrunache@gmail.com>
2016-05-15 20:34:23 -0400

    added home page for data verification

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
   end
+  def verify_user (params)
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to:'pages#home'
-
+  get 'verify', to: 'pages#verify_user'
   resources :departments
   resources :positions
   resources :job_statuses
